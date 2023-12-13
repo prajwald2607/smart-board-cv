8c35c37f4826b037f9dba91da5d73c7be6e5140a
Prajwal Deshmukh <prajwal.deshmukh@mitaoe.ac.in>
2023-12-14 01:22:20 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@
 # Install system dependencies
 sudo apt-get update
 sudo apt-get install -y libgl1-mesa-glx
-sudo apt install libgl1-mesa-glx
+RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 
 # Install Python dependencies
 pip install -r requirements.txt
